25c58ee9c946fca80515a812dd61163006f102ea
RemaSharaf <remmasharaf@gmail.com>
2023-04-19 14:59:27 +0300

    commit

--- lib/view/main/main.dart
+++ lib/view/main/main.dart
@@ -40,99 +40,73 @@ class Main extends GetView<HomeController> {
                             child: e["Widget"],
                           ))
                       .toList()),
-              bottomNavigation()
             ],
           ),
+          bottomNavigationBar: BottomNavigationBar(
+              showSelectedLabels: true,
+              selectedLabelStyle: TextStyle(
+                color: orange,
+                fontSize: 12.sp,
+              ),
+              onTap: (value) {
+                controller.tabIndex.value = value;
+              },
+              items: controller.widgets
+                  .map(
+                    (e) => BottomNavigationBarItem(
+                      backgroundColor: Colors.white,
+                      label: e["title"],
+                      icon: Icon(
+                        e["icon"],
+                        color: controller.tabIndex.value == e["id"]
+                            ? orange
+                            : hint,
+                      ),
+                    ),
+                  )
+                  .toList()),
           floatingActionButton: FloatingActionButton(
             backgroundColor: orange,
             child: const Icon(Icons.apps),
             onPressed: () {},
           ),
           floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
+              FloatingActionButtonLocation.centerDocked,
         ));
   }
 }
 
-Widget bottomNavigation() {
-  int index;
-  final controller = Get.find<HomeController>();
-  return Positioned(
-    bottom: 0,
-    left: 0,
-    right: 0,
-    child: Container(
-      height: 53.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Colors.white,
-      ),
-      child: Obx(() => Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children:
-                // homeCtrl.widgets
-                //     .map(
-                //       (e) => IconButton(onPressed: () {}, icon: Icon(e["icon"])),
-                //     )
-                //     .toList()
-                [
-              IconButton(
-                onPressed: () {
-                  index = 0;
-                  controller.changeTab(index);
-                },
-                icon: Icon(
-                  Icons.home,
-                  color: controller.tabIndex == 0 ? orange : hint,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  index = 1;
-                  controller.changeTab(index);
-                },
-                icon: Icon(
-                  Icons.heart_broken_outlined,
-                  color: controller.tabIndex == 1 ? orange : hint,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  index = 2;
-                  controller.changeTab(index);
-                },
-                icon: Icon(
-                  Icons.pie_chart_outline,
-                  color: controller.tabIndex == 2 ? orange : hint,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  index = 3;
-                  controller.changeTab(index);
-                },
-                icon: Icon(Icons.person_outline,
-                    color: controller.tabIndex == 3 ? orange : hint),
-              ),
-            ],
-          )),
-    ),
-  );
-}
-
-// Widget iconBottom(int index, controller, Title) {
-//   return IconButton(
-//     onPressed: () {
-//       controller.changeTab(index);
-//     },
-//     icon: Row(
-//       children: [
-//         Icon(
-//           Icons.home,
-//           color: controller.tabIndex == index ? orange : hint,
+// Widget bottomNavigation() {
+//   int index;
+//   final controller = Get.find<HomeController>();
+//   return Positioned(
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+//     child: Container(
+//         height: 53.h,
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(15),
+//           color: Colors.white,
 //         ),
-//         controller.tabIndex == index ? Text("data") : Container()
-//       ],
-//     ),
+//         child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: controller.widgets
+//                 .map(
+//                   (e) => IconButton(
+//                     onPressed: () {
+//                       controller.changeTab(e["id"]);
+//                     },
+//                     icon: Icon(
+//                       e["icon"],
+//                       color:
+//                           controller.tabIndex.value == e["id"] ? orange : hint,
+//                     ),
+//                   ),
+//                 )
+//                 .toList()
+         
+//             )),
 //   );
 // }
+
